7875daec3256483c07807ec437f2c6c93616a772
Leon Coto <leoncoto@gmail.com>
2025-08-22 21:18:59 +0100

    show that line and subscriptions are the same

--- experiment-02/src/ClockAndLine.tsx
+++ experiment-02/src/ClockAndLine.tsx
@@ -1,30 +1,58 @@
 import { useState } from "react";
 import { clockAndLine } from "./shared-worker/clock";
 
+const toTime = (value: number) => new Date(value).toLocaleTimeString();
+
 export const ClockAndLine = () => {
   const [closeLine, setLine] = useState<() => void>();
+  const [unsubscribe, setUnsubscribe] = useState<() => void>();
+
   return (
     <div>
-      {!closeLine && (
+      {unsubscribe ? (
         <button
-          onClick={async () => {
-            const line = await clockAndLine.openLine((time) => {
-              console.log("time", new Date(time).toLocaleTimeString());
-            });
-            setLine(() => line);
+          style={{ margin: 5 }}
+          onClick={() => {
+            unsubscribe();
+            setUnsubscribe(undefined);
           }}
         >
-          Open Line
+          Subscribe from time
+        </button>
+      ) : (
+        <button
+          onClick={async () => {
+            const unsubscribe = await clockAndLine.time((time) => {
+              console.log("time from subs", toTime(time));
+            });
+            setUnsubscribe(() => unsubscribe);
+          }}
+          style={{ margin: 5 }}
+        >
+          Subscribe to time
         </button>
       )}
-      {closeLine && (
+      {closeLine ? (
         <button
+          style={{ margin: 5 }}
           onClick={() => {
             closeLine();
             setLine(undefined);
           }}
         >
           Close Line
+        </button>
+      ) : (
+        <button
+          style={{ margin: 5 }}
+          onClick={async () => {
+            const line = await clockAndLine.openLine((time) => {
+              console.log("time from line", toTime(time));
+            });
+            setLine(() => line);
+          }}
+        >
+          Open Line
         </button>
       )}
     </div>
